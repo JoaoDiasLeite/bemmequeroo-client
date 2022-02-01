e052cce6e1010fb71a8da2c008bce27c4111bb56
João Leite <joao.leite@wiremaze.com>
2022-02-01 21:16:12 +0000

    Final Version

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -51,6 +51,8 @@ class Contact extends Component {
                 headers: {
                   "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
                   "Access-Control-Allow-Origin": "*",
+                  "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+                  "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token"
                 }
               },{withCredentials:true})
       .then(alert('Pedido enviado' ), response => { console.log(response.data); })
